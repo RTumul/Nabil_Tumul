931dffc6f0eb896a5bfb96c19f3705c87298a91d
RTumul <58302522+RTumul@users.noreply.github.com>
2021-08-05 19:36:21 +0600

    Add files via upload

--- eBook_service/Home_Delivery.php
+++ eBook_service/Home_Delivery.php
@@ -11,20 +11,174 @@
 	</head>
 	<body>
 				  				    
-			<center><img src="logo2.png" style="height:100px"></center><br/>
+		<center><img src="logo2.png" style="height:100px"></center>
+		<h4 style="text-align:right;">User Name: <?php echo $_SESSION["uname"];?></h4>
+	    <h5 style="text-align:right;">User Type: Delivery Man</h5>
 			      
 	<form action="" method="post">
 		<div class="text-center">
-			<a href="#" class="btn btn-primary">Available Work</a>
+			<a href="#" class="btn btn-primary">My Job</a>
 			<a href="deliveryman_profile_info.php" class="btn btn-warning">Edit Information</a>
 			<a href="deleteDeliverymanProfile.php" class="btn btn-danger">Delete Account</a>
 			<a href="loginDeliveryMan.php" class="btn btn-warning">Logout</a>		
 		</div>
-		</fieldset>
+		</fieldset><br/><br/>
+	<div class="col-md-12">
+	<div class="card-product col-md-4">
+		<a href="item.php">
+			<div><img class="card-image" src="bookimg.jpg"></img></div>
+			<b class="text"> Sample Book</b>
+		</a>
+		<div class="pick-add"><span ><b>Pickup Address: ABC</b></span></div>
+		<div class="del-add"><span ><b>Delivery Address: DEF</b></span></div>
+		<div class="add-to-cart"><a class="btn btn-success" style="width:185px;font-family:consolas;margin-top:5px;">Take This Job</a></span></div>
+		
+	</div>
+	<div class="card-product col-md-4">
+		<a href="item.php">
+			<div><img class="card-image" src="bookimg.jpg"></img></div>
+			<b class="text"> Sample Book</b>
+		</a>
+		<div class="pick-add"><span ><b>Pickup Address: ABC</b></span></div>
+		<div class="del-add"><span ><b>Delivery Address: DEF</b></span></div>
+		<div class="add-to-cart"><a class="btn btn-success" style="width:185px;font-family:consolas;margin-top:5px;">Take This Job</a></span></div>
+		
+	</div>
+	<div class="card-product col-md-4">
+		<a href="item.php">
+			<div><img class="card-image" src="bookimg.jpg"></img></div>
+			<b class="text"> Sample Book</b>
+		</a>
+		<div class="pick-add"><span ><b>Pickup Address: ABC</b></span></div>
+		<div class="del-add"><span ><b>Delivery Address: DEF</b></span></div>
+		<div class="add-to-cart"><a class="btn btn-success" style="width:185px;font-family:consolas;margin-top:5px;">Take This Job</a></span></div>
+		
+	</div>
+	<div class="card-product col-md-4">
+		<a href="item.php">
+			<div><img class="card-image" src="bookimg.jpg"></img></div>
+			<b class="text"> Sample Book</b>
+		</a>
+		<div class="pick-add"><span ><b>Pickup Address: ABC</b></span></div>
+		<div class="del-add"><span ><b>Delivery Address: DEF</b></span></div>
+		<div class="add-to-cart"><a class="btn btn-success" style="width:185px;font-family:consolas;margin-top:5px;">Take This Job</a></span></div>
+		
+	</div>
+	<div class="card-product col-md-4">
+		<a href="item.php">
+			<div><img class="card-image" src="bookimg.jpg"></img></div>
+			<b class="text"> Sample Book</b>
+		</a>
+		<div class="pick-add"><span ><b>Pickup Address: ABC</b></span></div>
+		<div class="del-add"><span ><b>Delivery Address: DEF</b></span></div>
+		<div class="add-to-cart"><a class="btn btn-success" style="width:185px;font-family:consolas;margin-top:5px;">Take This Job</a></span></div>
+		
+	</div>
+	<div class="card-product col-md-4">
+		<a href="item.php">
+			<div><img class="card-image" src="bookimg.jpg"></img></div>
+			<b class="text"> Sample Book</b>
+		</a>
+		<div class="pick-add"><span ><b>Pickup Address: ABC</b></span></div>
+		<div class="del-add"><span ><b>Delivery Address: DEF</b></span></div>
+		<div class="add-to-cart"><a class="btn btn-success" style="width:185px;font-family:consolas;margin-top:5px;">Take This Job</a></span></div>
+		
+	</div>
+	<div class="card-product col-md-4">
+		<a href="item.php">
+			<div><img class="card-image" src="bookimg.jpg"></img></div>
+			<b class="text"> Sample Book</b>
+		</a>
+		<div class="pick-add"><span ><b>Pickup Address: ABC</b></span></div>
+		<div class="del-add"><span ><b>Delivery Address: DEF</b></span></div>
+		<div class="add-to-cart"><a class="btn btn-success" style="width:185px;font-family:consolas;margin-top:5px;">Take This Job</a></span></div>
+		
+	</div>
+	<div class="card-product col-md-4">
+		<a href="item.php">
+			<div><img class="card-image" src="bookimg.jpg"></img></div>
+			<b class="text"> Sample Book</b>
+		</a>
+		<div class="pick-add"><span ><b>Pickup Address: ABC</b></span></div>
+		<div class="del-add"><span ><b>Delivery Address: DEF</b></span></div>
+		<div class="add-to-cart"><a class="btn btn-success" style="width:185px;font-family:consolas;margin-top:5px;">Take This Job</a></span></div>
+		
+	</div>
+	<div class="card-product col-md-4">
+		<a href="item.php">
+			<div><img class="card-image" src="bookimg.jpg"></img></div>
+			<b class="text"> Sample Book</b>
+		</a>
+		<div class="pick-add"><span ><b>Pickup Address: ABC</b></span></div>
+		<div class="del-add"><span ><b>Delivery Address: DEF</b></span></div>
+		<div class="add-to-cart"><a class="btn btn-success" style="width:185px;font-family:consolas;margin-top:5px;">Take This Job</a></span></div>
+		
+	</div>
+	<div class="card-product col-md-4">
+		<a href="item.php">
+			<div><img class="card-image" src="bookimg.jpg"></img></div>
+			<b class="text"> Sample Book</b>
+		</a>
+		<div class="pick-add"><span ><b>Pickup Address: ABC</b></span></div>
+		<div class="del-add"><span ><b>Delivery Address: DEF</b></span></div>
+		<div class="add-to-cart"><a class="btn btn-success" style="width:185px;font-family:consolas;margin-top:5px;">Take This Job</a></span></div>
+		
+	</div>
+	<div class="card-product col-md-4">
+		<a href="item.php">
+			<div><img class="card-image" src="bookimg.jpg"></img></div>
+			<b class="text"> Sample Book</b>
+		</a>
+		<div class="pick-add"><span ><b>Pickup Address: ABC</b></span></div>
+		<div class="del-add"><span ><b>Delivery Address: DEF</b></span></div>
+		<div class="add-to-cart"><a class="btn btn-success" style="width:185px;font-family:consolas;margin-top:5px;">Take This Job</a></span></div>
+		
+	</div>
+	<div class="card-product col-md-4">
+		<a href="item.php">
+			<div><img class="card-image" src="bookimg.jpg"></img></div>
+			<b class="text"> Sample Book</b>
+		</a>
+		<div class="pick-add"><span ><b>Pickup Address: ABC</b></span></div>
+		<div class="del-add"><span ><b>Delivery Address: DEF</b></span></div>
+		<div class="add-to-cart"><a class="btn btn-success" style="width:185px;font-family:consolas;margin-top:5px;">Take This Job</a></span></div>
+		
+	</div>
+	<div class="card-product col-md-4">
+		<a href="item.php">
+			<div><img class="card-image" src="bookimg.jpg"></img></div>
+			<b class="text"> Sample Book</b>
+		</a>
+		<div class="pick-add"><span ><b>Pickup Address: ABC</b></span></div>
+		<div class="del-add"><span ><b>Delivery Address: DEF</b></span></div>
+		<div class="add-to-cart"><a class="btn btn-success" style="width:185px;font-family:consolas;margin-top:5px;">Take This Job</a></span></div>
+		
+	</div>
+	<div class="card-product col-md-4">
+		<a href="item.php">
+			<div><img class="card-image" src="bookimg.jpg"></img></div>
+			<b class="text"> Sample Book</b>
+		</a>
+		<div class="pick-add"><span ><b>Pickup Address: ABC</b></span></div>
+		<div class="del-add"><span ><b>Delivery Address: DEF</b></span></div>
+		<div class="add-to-cart"><a class="btn btn-success" style="width:185px;font-family:consolas;margin-top:5px;">Take This Job</a></span></div>
+		
+	</div>
+	<div class="card-product col-md-4">
+		<a href="item.php">
+			<div><img class="card-image" src="bookimg.jpg"></img></div>
+			<b class="text"> Sample Book</b>
+		</a>
+		<div class="pick-add"><span ><b>Pickup Address: ABC</b></span></div>
+		<div class="del-add"><span ><b>Delivery Address: DEF</b></span></div>
+		<div class="add-to-cart"><a class="btn btn-success" style="width:185px;font-family:consolas;margin-top:5px;">Take This Job</a></span></div>
+		
+	</div>
+	</div>
+	
 		
 	
 	
-	<br/><br/><br/><br/><hr/><h4 style="text-align:left;">User Name: <?php echo $_SESSION["uname"];?></h4>
-	<h5 style="text-align:left;">User Type: Delivery Man</h5>
+	
 	
 	</html>
