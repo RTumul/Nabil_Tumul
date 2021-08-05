20c8e44252d553b52e447d7077ab031d9fe4f3c6
RTumul <58302522+RTumul@users.noreply.github.com>
2021-08-05 20:03:33 +0600

    Add files via upload

--- ebook_service.sql
+++ ebook_service.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 03, 2021 at 11:29 PM
+-- Generation Time: Aug 05, 2021 at 03:59 PM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 8.0.6
 
@@ -20,6 +20,28 @@ SET time_zone = "+00:00";
 --
 -- Database: `ebook_service`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books_info`
+--
+
+CREATE TABLE `books_info` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `wname` varchar(50) NOT NULL,
+  `pri` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone` int(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `books_info`
+--
+
+INSERT INTO `books_info` (`id`, `name`, `wname`, `pri`, `email`, `phone`) VALUES
+(1, 'Jamai Raja', 'Kolkata', 200, 'meh.rubu@yahoo.com', 1725103018);
 
 -- --------------------------------------------------------
 
@@ -43,7 +65,8 @@ CREATE TABLE `deliveryman_info` (
 --
 
 INSERT INTO `deliveryman_info` (`id`, `name`, `uname`, `pass`, `email`, `phone`, `birthdate`, `gender`) VALUES
-(1, 'Md Ridwanuzzaman', 'Tumul123', 'Tumul#1234', 'amitumul@gmail.com', 1725103018, '3/Mar(3)/2', 'Male');
+(5, 'Turno', 'Turno123', 'Turno#123', 'turno@gmail.com', 1725103018, '3/Mar(3)/1', 'Female'),
+(8, 'Demo', 'Demo_User', 'Demo#123', 'meh.rubu@yahoo.com', 1725103018, '1/Jan(1)/1', 'Male');
 
 -- --------------------------------------------------------
 
@@ -52,27 +75,34 @@ INSERT INTO `deliveryman_info` (`id`, `name`, `uname`, `pass`, `email`, `phone`,
 --
 
 CREATE TABLE `seller_info` (
-  `ID` int(11) NOT NULL,
-  `Name` varchar(50) NOT NULL,
-  `Username` varchar(50) NOT NULL,
-  `Password` varchar(50) NOT NULL,
-  `Email` varchar(50) NOT NULL,
-  `Phone_Number` varchar(15) NOT NULL,
-  `Address` varchar(50) NOT NULL,
-  `Gender` varchar(6) NOT NULL,
-  `Date_of_Birth` varchar(15) NOT NULL
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `uname` varchar(50) NOT NULL,
+  `pass` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `birthdate` varchar(10) NOT NULL,
+  `gender` varchar(50) NOT NULL,
+  `address` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `seller_info`
 --
 
-INSERT INTO `seller_info` (`ID`, `Name`, `Username`, `Password`, `Email`, `Phone_Number`, `Address`, `Gender`, `Date_of_Birth`) VALUES
-(402, 'Mahbuba Sharmin Ruba', 'Ruba123', 'Tumul123', 'amitumul@gmail.com', '01725103018', 'House-17, Road- 5, Block- C, Pallabi,  Mirpur 12', 'Male', '12/03/2000');
+INSERT INTO `seller_info` (`id`, `name`, `uname`, `pass`, `email`, `phone`, `birthdate`, `gender`, `address`) VALUES
+(1, 'Tumul', 'Tumul156', '', 'meh.rubu@yahoo.com', 1725103018, '1/Jan(1)/1', 'Male', 'House-17, Road- 5, Block- C, Pallabi,  Mirpur 12'),
+(2, 'Turno', 'Turno123', '', 'meh@yahoo.com', 1725103018, '1/Jan(1)/1', 'Female', 'House-17, Road- 5, Block- C, Pallabi,  Mirpur 12');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `books_info`
+--
+ALTER TABLE `books_info`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `deliveryman_info`
@@ -85,24 +115,30 @@ ALTER TABLE `deliveryman_info`
 -- Indexes for table `seller_info`
 --
 ALTER TABLE `seller_info`
-  ADD PRIMARY KEY (`ID`),
-  ADD UNIQUE KEY `Username` (`Username`,`Email`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uname` (`uname`,`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `books_info`
+--
+ALTER TABLE `books_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `deliveryman_info`
 --
 ALTER TABLE `deliveryman_info`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `seller_info`
 --
 ALTER TABLE `seller_info`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=403;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
